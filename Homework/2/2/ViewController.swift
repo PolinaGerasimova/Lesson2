9b58bc1986f99333b2df1a1f6592c36889a09eaa
PolinaGerasimova <74964222+PolinaGerasimova@users.noreply.github.com>
2020-11-30 22:38:33 +0500

    Donload change of homework

---
+++ Homework/2/2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  2
+//
+//  Created by USER on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tekst: UILabel!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBAction func button(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
